51c5719fc43aa781e3f7f2ffc97fac9683aa304c
ShikouMochizuki <mochi45job@gmail.com>
2024-05-16 18:03:26 +0900

    fix : LlamaTest.csを実行するように変更

--- test/ci/run.sh
+++ test/ci/run.sh
@@ -1,10 +1,9 @@
 set -e 
 pushd test/ci/
 export PATH=$HOME/.dotnet:$PATH
-cp ../TestLlamaCppUnity/Assets/Scripts/LlamaSample.cs Program.cs
-cp -r ../TestLlamaCppUnity/Assets/Scripts/test .
+cp ../TestLlamaCppUnity/Assets/Scripts/LlamaTest.cs Program.cs
 cp -r ../../Packages/Runtime .
-mv ../TestLlamaCppUnity/Assets/Models/llama-2-7b-chat.ggml.q2_K.gguf Assets/Models/
+mv ../TestLlamaCppUnity/Assets/Models/llama-2-7b-chat.q2_K.gguf Assets/Models/
 ls 
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 dotnet build 
