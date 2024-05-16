2e27ec24da23d322b4a30e8ea3b7c3a013e67fcf
ShikouMochizuki <mochi45job@gmail.com>
2024-05-16 17:47:48 +0900

    fix : pathをTestLlamaCppUnityに変更

--- test/ci/run.sh
+++ test/ci/run.sh
@@ -1,10 +1,10 @@
 set -e 
 pushd test/ci/
 export PATH=$HOME/.dotnet:$PATH
-cp ../CSLlamaSampleProject/Assets/Scripts/LlamaSample.cs Program.cs
-cp -r ../CSLlamaSampleProject/Assets/Scripts/test .
+cp ../TestLlamaCppUnity/Assets/Scripts/LlamaSample.cs Program.cs
+cp -r ../TestLlamaCppUnity/Assets/Scripts/test .
 cp -r ../../Packages/Runtime .
-mv ../CSLlamaSampleProject/Assets/Models/llama-2-7b-chat.ggml.q2_K.gguf Assets/Models/
+mv ../TestLlamaCppUnity/Assets/Models/llama-2-7b-chat.ggml.q2_K.gguf Assets/Models/
 ls 
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 dotnet build 
