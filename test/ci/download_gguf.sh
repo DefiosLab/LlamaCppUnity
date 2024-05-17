db32369071be4a3c9d473ab42633b73afbffa664
ShikouMochizuki <mochi45job@gmail.com>
2024-05-17 10:09:12 +0900

    fix : delete unnecessary processing

--- test/ci/download_gguf.sh
+++ test/ci/download_gguf.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 set -e
 pushd $CI_PROJECT_DIR/.cache/
-# huggingfaceディレクトリが存在しなければ作成
-if [ ! -d "huggingface" ]; then
-    mkdir -p huggingface
-fi
-# huggingfaceディレクトリに移動
-cd huggingface
-
 if [ ! -f "llama-2-7b-chat.Q2_K.gguf" ]; then
     wget -q https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q2_K.gguf?download=true -O llama-2-7b-chat.Q2_K.gguf
-    cd ../
 fi
-mv $CI_PROJECT_DIR/.cache/huggingface/llama-2-7b-chat.Q2_K.gguf $CI_PROJECT_DIR/test/ci/Assets/Models
+mv $CI_PROJECT_DIR/.cache/llama-2-7b-chat.Q2_K.gguf $CI_PROJECT_DIR/test/ci/Assets/Models
 popd
