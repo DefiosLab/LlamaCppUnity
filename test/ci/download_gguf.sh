034e5014b970b4a9aff17077db4fe144ffc72fb7
ShikouMochizuki <mochi45job@gmail.com>
2024-05-23 15:17:38 +0900

    fix: remove Assets/Models directory

--- test/ci/download_gguf.sh
+++ test/ci/download_gguf.sh
@@ -8,5 +8,5 @@ fi
 if [ ! -f "llama-2-7b-chat.Q2_K.gguf" ]; then
     wget -q https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q2_K.gguf?download=true -O llama-2-7b-chat.Q2_K.gguf
 fi
-mv ${GITHUB_WORKSPACE}/.cache/llama-2-7b-chat.Q2_K.gguf ${GITHUB_WORKSPACE}/test/ci/Assets/Models/
+mv llama-2-7b-chat.Q2_K.gguf ../test/ci
 popd
