db598ee859b8a41b155b3eb910e75d3b9bbaea14
ShikouMochizuki <mochi45job@gmail.com>
2024-05-23 14:41:28 +0900

    fix: adjust to work with both GitLab and GitHub

--- test/ci/download_gguf.sh
+++ test/ci/download_gguf.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
-pushd "${GITHUB_WORKSPACE}/.cache/"
+if [ -n "${GITHUB_WORKSPACE}" ]; then
+    pushd "${GITHUB_WORKSPACE}/.cache"
+elif [ -n "${CI_PROJECT_DIR}" ]; then
+    pushd "${CI_PROJECT_DIR}/.cache"
+fi
 if [ ! -f "llama-2-7b-chat.Q2_K.gguf" ]; then
     wget -q https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q2_K.gguf?download=true -O llama-2-7b-chat.Q2_K.gguf
 fi
